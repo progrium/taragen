6de4edde8fe937aefd181bcc4efb18e17319b530
taramk <taramkirkland@gmail.com>
2025-02-28 11:52:22 -0800

    web: import treehouse CSS and set up base layout/styles

--- web/docs/_layout.jsx
+++ web/docs/_layout.jsx
@@ -1,25 +1,28 @@
 (content) =>
-<div>
-<nav>
-    <ul>
-    {pages("docs").map(page => (
-        <li>
-            <a href={page.path}>{page.title}</a>
-            {page.isDir && 
-                <ul>
-                    {pages(page.path).map(sub => (
-                        <li>
-                            <a href={sub.path}>{sub.title}</a>
-                        </li>
-                    ))}
-                </ul>
-            }
-        </li>
-    ))}
-    </ul>
-</nav>
-<main>
-    {content}
-</main>
-</div>
+<section>
+    <div class="row justify-center items-start sm:stack" style="gap: var(--16)">
+        <nav class="flex: none; min-width: 256px;">
+            <ul>
+            {pages("docs").map(page => (
+                <li>
+                    <a href={page.path}>{page.title}</a>
+                    {page.isDir && 
+                        <ul>
+                            {pages(page.path).map(sub => (
+                                <li>
+                                    <a href={sub.path}>{sub.title}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    }
+                </li>
+            ))}
+            </ul>
+        </nav>
+
+        <main class="grow">
+            {content}
+        </main>
+    </div>
+</section>
     
